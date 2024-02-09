058e9acea3b94c356c3b0e6902361f7609444e69
ebadwebdev9231 <kebad9231@gmail.com>
2024-02-09 18:56:50 +0500

    change text animation

--- src/components/AnimatedText.jsx
+++ src/components/AnimatedText.jsx
@@ -2,23 +2,15 @@ import "./AnimatedText.css";
 const AnimatedText = () => {
   return (
     <div className="animated-text">
-      <h1>
-        <span style={{ "--i": 1 }}>H</span>
-        <span style={{ "--i": 2 }}>a</span>
-        <span style={{ "--i": 3 }}>p</span>
-        <span style={{ "--i": 4 }}>p</span>
-        <span style={{ "--i": 5 }}>y</span>
-      </h1>
-      <h1 className="break">
-        <span style={{ "--i": 6 }}>B</span>
-        <span style={{ "--i": 7 }}>i</span>
-        <span style={{ "--i": 8 }}>r</span>
-        <span style={{ "--i": 9 }}>t</span>
-        <span style={{ "--i": 10 }}>h</span>
-        <span style={{ "--i": 11 }}>d</span>
-        <span style={{ "--i": 12 }}>a</span>
-        <span style={{ "--i": 13 }}>y</span>
-      </h1>
+      <h2>Happy Birthday</h2>
+      <h3 className="break">
+        <span>N</span>
+        <span>A</span>
+        <span>H</span>
+        <span>I</span>
+        <span>M</span>
+        <span>A</span>
+      </h3>
     </div>
   );
 };
